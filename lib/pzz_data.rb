9f408a0e8cc75f08efe759570847c648e2bd8563
kot <miseinen22@gmail.com>
2021-05-09 08:54:51 +0300

    Add PzzData

---
+++ lib/pzz_data.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'net/http'
+
+class PzzData
+  attr_reader :data
+
+  def initialize
+    source = 'https://pzz.by/api/v1/pizzas?load=ingredients,filters&filter=meal_only:0&order=position:asc'
+    response = Net::HTTP.get_response(URI.parse(source))
+    body = response.body
+
+    @data = JSON.parse(body)
+  end
+end
